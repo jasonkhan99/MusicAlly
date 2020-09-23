80ffb1296cc981ce3d611897e870914404d653fb
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-23 11:32:25 -0700

    add musiclist.js

---
+++ components/MusicList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Music from "./Music";
+
+function MusicList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+      {Object.values(props.musicList).map((music) => {
+        return <Music
+          whenMusicClicked = { props.onMusicSelection }
+          trackName = { music.trackName } 
+          trackEdit = { music.trackEdit }
+          actName = { music.actName }
+          featureNames = { props.featureNames }
+          artistNames = { props.artistNames }
+          releaseName = { props.releaseName }
+          releaseYear = { props.releaseYear }
+          labelName = { props.labeName }
+          labelNumber = { props.labelNumber }
+          trackLength = { props.trackLength }
+          bpm = { props.bpm }
+          format = { props.format }
+          genre = { props.genre }
+          styles = { props.styles }
+          actName = { props.actName }
+          descriptionTags = { props.descriptionTags } 
+          id = { music.id }
+          key = { music.id } />
+      })}
+    </React.Fragment>
+  );
+}
+
+MusicList.propTypes = {
+  musicList = PropTypes.object,
+  onMusicSelection = PropTypes.func
+};
+
+export default MusicList;
