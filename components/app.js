2a001bcbbe4228a92395cb049e1f90e357023a16
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-23 09:17:49 -0700

    add app.js

---
+++ components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import MusicControl from "./MusicControl";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <MusicControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
