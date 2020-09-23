494e008768784d873773c8812a0a2b6419998ea3
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-23 11:22:17 -0700

    add editmusicform.js

---
+++ components/EditMusicForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditMusicForm (props) {
+  const { music } = props;
+
+  function handleEditMusicFormSubmission(event) {
+    event.preventDefault();
+    props.onEditMusic({
+      trackName: event.target.trackName.value,
+      editName: event.target.editName.value,
+      actName: event.target.actName.value,
+      featureNames: event.target.featureNames.value,
+      artistNames: event.target.artistNames.value,
+      releaseName: event.target.releaseName.value,
+      releaseYear: event.target.releaseYear.value,
+      labelName: event.target.labeName.value,
+      labelNumber: event.target.labelNumber.value,
+      trackLength: event.target.trackLength.value,
+      bpm: event.target.bpm.value,
+      format: event.target.format.value,
+      genre: event.target.genre.value,
+      styles: event.target.styles.value,
+      actName: event.target.actName.value,
+      descriptionTags: event.target.descriptionTags.value, 
+      id: music.id
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditMusicFormSubmission}
+        buttonText="Update Music" />
+    </React.Fragment>
+  );
+}
+
+EditMusicForm.propTypes = {
+  onEditMusic: PropTypes.func
+};
+
+export default EditMusicForm;
