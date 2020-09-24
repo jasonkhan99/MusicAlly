b5922a81bed5e99d840477e664cdeff7f65ee77d
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-24 11:32:19 -0700

    update new music form to add to firestore

--- src/components/NewMusicForm.js
+++ src/components/NewMusicForm.js
@@ -1,37 +1,41 @@
 import React from "react";
-import { v4 } from 'uuid';
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
+import { useFirestore } from 'react-redux-firebase';
 
 function NewMusicForm(props){
 
-  function handleNewMusicFormSubmission(event) {
+  const firestore = useFirestore();
+
+  function addMusicToFirestore(event) {
     event.preventDefault();
-    props.onNewMusicCreation({
-      trackName: event.target.trackName.value,
-      editName: event.target.editName.value,
-      actName: event.target.actName.value,
-      featureNames: event.target.featureNames.value,
-      artistNames: event.target.artistNames.value,
-      releaseName: event.target.releaseName.value,
-      releaseYear: event.target.releaseYear.value,
-      labelName: event.target.labelName.value,
-      labelNumber: event.target.labelNumber.value,
-      trackLengthMinutes: event.target.trackLengthMinutes.value,
-      trackLengthSeconds: event.target.trackLengthSeconds.value,
-      bpm: event.target.bpm.value,
-      format: event.target.format.value,
-      genre: event.target.genre.value,
-      styles: event.target.styles.value,
-      descriptionTags: event.target.descriptionTags.value,
-      id: v4(),
-    });
+    props.onNewMusicCreation();
+    return firestore.collection('music').add(
+      {
+        trackName: event.target.trackName.value,
+        editName: event.target.editName.value,
+        actName: event.target.actName.value,
+        featureNames: event.target.featureNames.value,
+        artistNames: event.target.artistNames.value,
+        releaseName: event.target.releaseName.value,
+        releaseYear: event.target.releaseYear.value,
+        labelName: event.target.labelName.value,
+        labelNumber: event.target.labelNumber.value,
+        trackLengthMinutes: event.target.trackLengthMinutes.value,
+        trackLengthSeconds: event.target.trackLengthSeconds.value,
+        bpm: event.target.bpm.value,
+        format: event.target.format.value,
+        genre: event.target.genre.value,
+        styles: event.target.styles.value,
+        descriptionTags: event.target.descriptionTags.value,
+      }
+    );
   }
 
   return (
     <React.Fragment>
       <ReusableForm 
-        formSubmissionHandler={handleNewMusicFormSubmission}
+        formSubmissionHandler={addMusicToFirestore}
         buttonText="Add Music" />
     </React.Fragment>
   );
