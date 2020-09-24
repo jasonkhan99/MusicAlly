b9ca81967d61ff7a612964c3f86243cb234f50c8
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-24 11:18:14 -0700

    add firestore reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from './form-visible-reducer';
 import musicListReducer from './music-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterMusicList: musicListReducer
+  masterMusicList: musicListReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
