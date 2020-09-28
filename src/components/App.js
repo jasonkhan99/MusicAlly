369802467bf6fa5da4578c53023827e181475e36
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-28 10:00:11 -0700

    update app.js to use routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import MusicControl from "./MusicControl";
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App(){
   return ( 
-    <React.Fragment>
+    <Router>
       <Header />
-      <MusicControl />
-    </React.Fragment>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <MusicControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
